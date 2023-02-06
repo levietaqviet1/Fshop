c984dfa0d3f36fb7fd08884cf6fa134baa5ee680
viet <85175337+levietaqviet1@users.noreply.github.com>
2023-02-06 23:57:54 +0700

    x

---
+++ code_be/buoi4/VietLD3_NPLC_Assignment4/Net.M.A009/Net.M.A009.Exercise2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Exercise2();
+    }
+
+    /// <summary>
+    /// Exercise2 
+    /// </summary>
+    public static void Exercise2()
+    {
+        string input = InputString("Input Email: ");
+        Console.WriteLine($"{input} is {CheckFormatEmail(input)}");
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="message"></param>
+    /// <returns></returns>
+    public static string InputString(string message)
+    {
+        Console.Write(message);
+        return Console.ReadLine().Trim(); // xóa khoảng trắng thừa đầu và cuỗi chuỗi
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="email"></param>
+    /// <returns></returns>
+    public static bool CheckFormatEmail(string email)
+    {
+        // ^[a-zA-Z0-9]+   Ký từ đầu từ a đến z hoặc A đến Z hoặc 0 đến 9 còn dấu + là có thể lặp lại 1 hoặc nhiều lần
+        // ?:\.[a-zA-Z0-9!#$%&'*+/=?^_`{|}~-]+   một hoặc nhiều ký tự [a-zA-Z0-9!#$%&'*+/=?^_`{|}~-] được cho phép trong phần sau dấu chấm
+        return Regex.IsMatch(email,
+         @"^[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$");
+
+
+    }
+}
---
+++ code_be/buoi4/VietLD3_NPLC_Assignment4/Net.M.A009/Net.M.A009.Exercise3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics.Metrics;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Exercise3();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public static void Exercise3()
+    {
+        string[] listUser = { "Tony Stark", "Steve Rogers", "Bruce Banner", "Thor", "Natasha Romanoff", "Clint Barton", "James Rhodes", "Scott Lang", "Doctor Strange", "Carol Danvers", "Peter Parker" };
+        listUser = SortListUser(listUser); // sắp xếp lại list
+        foreach (var item in listUser)
+        {
+            Console.Write($"{item}, "); // in danh sách ra
+        }
+    }
+
+    /// <summary>
+    /// Hàm sắp xếp
+    /// </summary>
+    /// <param name="listUser"></param>
+    /// <returns></returns>
+    public static string[] SortListUser(string[] listUser)
+    {
+        string temp = ""; // tạo biến trung gian
+        for (int i = 0; i < listUser.Length; i++)
+        {
+            for (int j = i; j < listUser.Length; j++)
+            {
+                if (GetLastName(listUser[i]).CompareTo(GetLastName(listUser[j])) > 0) // dùng CompareTo để so sánh 
+                {
+                    temp = listUser[i];
+                    listUser[i] = listUser[j];
+                    listUser[j] = temp;
+                }
+            }
+        }
+
+        return listUser; // trả về list đã đc sắp xếp
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns></returns>
+    public static string GetLastName(string name)
+    {
+        string[] arr = name.Split(" "); // dùng Split để tách chuỗi
+
+        return arr[arr.Length-1]; // lấy phần tử cuối cùng là last name
+    }
+}
