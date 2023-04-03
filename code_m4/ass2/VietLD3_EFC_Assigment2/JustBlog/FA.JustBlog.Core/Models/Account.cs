25659302d7f7a66d149c2f5632ac95be0d9bf2ad
levietaqviet1 <levietaqviet2001@gmail.com>
2023-04-03 17:10:21 +0700

    'x'

---
+++ code_m4/ass2/VietLD3_EFC_Assigment2/JustBlog/FA.JustBlog.Core/Models/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace FA.JustBlog.Core.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+
+        public int MyProperty { get; set; }
+    }
+}
--- code_m4/ass2/VietLD3_EFC_Assigment2/JustBlog/FA.JustBlog/Controllers/PostController.cs
+++ code_m4/ass2/VietLD3_EFC_Assigment2/JustBlog/FA.JustBlog/Controllers/PostController.cs
@@ -1,17 +1,91 @@
-﻿using FA.JustBlog.Core.Models;
-using FA.JustBlog.Core.Repositories;
-using FA.JustBlog.Core.Repositories.Impl;
+﻿using FA.JustBlog.Core.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FA.JustBlog.Controllers
 {
+
     public class PostController : Controller
     {
-        public IActionResult Index()
+        private readonly IPostRepository _postRepository;
+
+        public PostController(IPostRepository postRepository)
         {
-            IPostRepository postRepository = new PostRepository();
-            List<Post> posts = postRepository.GetAll().ToList();
-            return View(posts);
+            _postRepository = postRepository;
+        }
+
+        // GET: PostController
+        public ActionResult Index()
+        {
+            return View(_postRepository.GetAll());
+        }
+
+        // GET: PostController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PostController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PostController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PostController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PostController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PostController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PostController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
